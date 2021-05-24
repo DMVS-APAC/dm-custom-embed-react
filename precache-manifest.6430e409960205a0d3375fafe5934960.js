480a70dbc12abb19754a5b0ba95ef279a8c38ac7
skhassandaily <sk.hassan@dailymotion.com>
2021-05-24 16:24:51 +0530

    Updates

---
+++ precache-manifest.6430e409960205a0d3375fafe5934960.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b2ade283227dd8f5910ad3ab5648b07",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f8199067bfab37ae43dc",
+    "url": "./static/css/main.bc2338c4.chunk.css"
+  },
+  {
+    "revision": "b0aef528d09823422d12",
+    "url": "./static/js/2.737a5569.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.737a5569.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8199067bfab37ae43dc",
+    "url": "./static/js/main.dcf24da5.chunk.js"
+  },
+  {
+    "revision": "378c3323fb574f15862e",
+    "url": "./static/js/runtime-main.f4014db3.js"
+  }
+]);
