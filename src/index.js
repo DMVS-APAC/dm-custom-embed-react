ad058f5489823da2017c1c02d04d62081c1d23b3
skhassandaily <sk.hassan@dailymotion.com>
2021-05-24 16:08:20 +0530

    first upload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React from 'react';
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+class DmCustomEmbed extends React.Component {
+
+    constructor(props) {
+        super(props);
+    
+        this.refContainer = (container) => {
+          this.container = container;
+        };
+    }
+    componentDidMount() {
+        const script = document.createElement("script");
+        script.src = "https://srvr.dmvs-apac.com/v2/dm-ce.min.js";
+        script.async = true;
+        document.body.appendChild(script);
+    }
+
+    render() {
+    
+        return (
+          <div
+            {...this.props}
+            className="dm-player"
+            ref={this.refContainer}
+          />
+        );
+    }
 }
+
+export default DmCustomEmbed;
--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,7 @@
-import { ExampleComponent } from '.'
+import DmCustomEmbed from '.'
 
-describe('ExampleComponent', () => {
+describe('DmCustomEmbed', () => {
   it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
+    expect(DmCustomEmbed).toBeTruthy()
   })
 })
